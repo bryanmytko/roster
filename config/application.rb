e676b95507540a1deae736af974e9cd4aa8fae6c
Bryan Mytko <bryanmytko@gmail.com>
2017-01-17 19:13:24 -0500

    Add eager load paths

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Guild
   class Application < Rails::Application
-    config.autoload_paths += %W(#{config.root}/lib)
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
-    config.autoload_paths << Rails.root.join('lib')
+    config.eager_load_paths += Dir["#{config.root}/lib/**/"]
   end
 end
