7702f489633d6d8df48856402a41c7e0487d22e3
Bryan Mytko <bryanmytko@gmail.com>
2017-01-19 21:47:34 -0500

    Add User model

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -4,4 +4,20 @@ class MembersController < ApplicationController
   def index
     @members = Member.all.order(ilvl_equipped: :desc)
   end
+
+  def new
+    @member = Member.new
+  end
+
+  def create
+    member = Member.create(member_params)
+    WowApi::Import.update_member_ilvl(member.name, member.realm)
+    redirect_to :root
+  end
+
+  private
+
+  def member_params
+    params.require(:member).permit(:name, :realm)
+  end
 end
