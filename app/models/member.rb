ecb9179efb2c74b476a6bd6aeafc1b3acd9dd157
Bryan Mytko <bryanmytko@gmail.com>
2017-01-20 15:07:18 -0500

    Add ilvl scope

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,8 @@
 class Member < ApplicationRecord
+  def update_ilvl(response)
+    update(
+      ilvl_display: response["items"]["averageItemLevel"],
+      ilvl_equipped: response["items"]["averageItemLevelEquipped"]
+    )
+  end
 end
